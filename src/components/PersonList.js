d99f33c4546faf20ca2fa4e4535ccf95c383fd17
Brett Cunningham <bwcunninghamii@gmail.com>
2018-08-26 07:04:22 -0400

    I added a debugger statement S7L32

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class PersonList extends Component {
+
+  componentWillMount() {
+    console.log('ComponentWillMount in PersonList');
+    debugger;
+  }
+
   render () {
     return (
       <View>
