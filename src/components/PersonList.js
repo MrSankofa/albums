88c7e03d99e5d828f5fd205860057ea8a54d0557
Brett Cunningham <bwcunninghamii@gmail.com>
2018-08-26 06:48:33 -0400

    Changed the PersonList component from functional to class to fetch data

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const PersonList = () => {
-  return (
-    <View>
-      <Text>Person List!!!!</Text>
-    </View>
-  );
-};
+class PersonList extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Person List!!!!</Text>
+      </View>
+    );
+  }
+}
 
 export default PersonList;
