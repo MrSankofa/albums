b7ad15d494a26621751b87374b1eda995a207540
Brett Cunningham <bwcunninghamii@gmail.com>
2018-08-26 08:48:30 -0400

    laid the foundation for creating PersonDetail. Working

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -8,8 +8,21 @@ class PersonList extends Component {
    axios.get('https://nodecopy.herokuapp.com/api/persons').then( response => this.setState({ Persons: response.data}) );
   }
 
+  /* Requires use for performance purposes whenever 
+  *  we use an array and render it to the screen to
+  *  assign a key value to the place where the array
+  *  element is defined. 
+  *  Rule 1: that key value must be unique
+  *  Rule 2: It also must be the same value for 
+  *          re-renders of the list. Ex. Don't use
+  *          person again as a key value.
+  *          ex. don't use person.name because people
+  *          could have the same name. It needs to be
+  *          unique whenever we log the data*/
   renderPerson() {
-    return this.state.Persons.map( person => <Text>{person.firstname}</Text>);
+    return this.state.Persons.map( person => 
+    <Text key={person._id}>{person.firstname}</Text>
+  );
   }
 
   render () {
